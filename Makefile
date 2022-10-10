b73af49ea9c6530772f16651c93fb758df808a6b
Davi Farhi <davifarhi@gmail.com>
2022-10-10 12:52:34 +0200

    added vector_op files

--- Makefile
+++ Makefile
@@ -6,13 +6,13 @@
 #    By: mreymond <mreymond@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/10/11 12:17:38 by dfarhi            #+#    #+#              #
-#    Updated: 2022/10/05 17:40:48 by davifah          ###   ########.fr        #
+#    Updated: 2022/10/10 12:31:07 by davifah          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 FILES		= minirt
 
-FILES_D		= mlx_setup mlx_utils colors_utils render_loop vector_rotation math_utils render_setup render_sphere render_per_pixel render_plane render_cylinder
+FILES_D		= mlx_setup mlx_utils colors_utils render_loop vector_rotation math_utils render_setup render_sphere render_per_pixel render_plane render_cylinder vector_op1 vector_op2
 FILES_M		= 	parsing parsing_setup parsing_tab parsing_volumes errors free \
 				display parsing_utils parsing_range
 
--- includes/minirt_math.h
+++ includes/minirt_math.h
@@ -6,7 +6,7 @@
 /*   By: davifah <dfarhi@student.42lausanne.ch      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 11:16:13 by davifah           #+#    #+#             */
-/*   Updated: 2022/09/07 12:05:16 by davifah          ###   ########.fr       */
+/*   Updated: 2022/10/10 12:30:12 by davifah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,16 @@
 
 double	calculate_degree_to_radians(double degrees);
 double	calculate_discriminant(const t_quadratic_equation *abc);
+
+// VECTORS
+t_vector	v_div(const t_vector v1, double n);
+t_vector	v_mult(const t_vector v1, double n);
+t_vector	v_sub(const t_vector v1, const t_vector v2);
+t_vector	v_add(const t_vector v1, const t_vector v2);
+t_vector	v_normalize(t_vector *v);
+t_vector	coord_to_vector(t_coord c);
+double		dot_product(const t_vector v1, const t_vector v2);
+double		distance(const t_coord *c1, const t_coord *c2);
+void		print_v(const t_vector v);
 
 #endif
