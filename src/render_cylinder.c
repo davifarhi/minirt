efa5ef7eeb451b07a4ac313cb7ca19bd15b94e4e
davifah <davifarhi@gmail.com>
2022-10-18 17:53:56 +0200

    cylinders added caps

--- src/render_cylinder.c
+++ src/render_cylinder.c
@@ -6,7 +6,7 @@
 /*   By: davifah <dfarhi@student.42lausanne.ch      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 16:17:20 by davifah           #+#    #+#             */
-/*   Updated: 2022/10/18 16:19:09 by dfarhi           ###   ########.fr       */
+/*   Updated: 2022/10/18 17:36:22 by dfarhi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,32 @@ double	cylinder_limit_height(const t_coord *ray_origin,
 	return (-1);
 }
 
-int	cylinder_calc_t_param_hit(double t[2])
+void	cylinder_caps(double t[2], const t_obj *obj,
+		const t_coord *ray_origin, const t_vector *v_ray)
+{
+	t_vector	po;
+	t_vector	p2;
+	double		pl_t;
+
+	t[0] = -1;
+	t[1] = -1;
+	pl_t = plane_intersection(*((t_cylinder *)obj->param)->vector,
+			*obj->coord, *v_ray, *ray_origin);
+	po = v_add(*(t_vector *)ray_origin, v_mult(*v_ray, pl_t));
+	if (fabs(distance((t_coord *)&po, obj->coord))
+		<= ((t_cylinder *)obj->param)->diameter / 2)
+		t[0] = pl_t;
+	p2 = v_add(*(t_vector *)obj->coord, v_mult(*((t_cylinder *)obj->param)
+				->vector, ((t_cylinder *)obj->param)->height));
+	pl_t = plane_intersection(*((t_cylinder *)obj->param)->vector,
+			*(t_coord *)&p2, *v_ray, *ray_origin);
+	po = v_add(*(t_vector *)ray_origin, v_mult(*v_ray, pl_t));
+	if (fabs(distance((t_coord *)&po, (t_coord *)&p2))
+		<= ((t_cylinder *)obj->param)->diameter / 2)
+		t[1] = pl_t;
+}
+
+int	cylinder_calc_t_param_hit(const double t[2])
 {
 	double	t_1;
 	double	t_2;
@@ -65,15 +90,15 @@ int	cylinder_calc_t_param_hit(double t[2])
 	if (t_1 < 0 && t_2 < 0)
 		return (0);
 	if (t_2 < 0)
-		return (1);
+		return (0);
 	else if (t_1 < 0)
-		return (2);
+		return (1);
 	else if (t_1 < t_2)
-		return (1);
+		return (0);
 	else if (t_2 < t_1)
-		return (2);
-	else
 		return (1);
+	else
+		return (0);
 }
 
 t_obj_ray_hit	*render_cylinder(const t_obj *obj,
@@ -82,6 +107,7 @@ t_obj_ray_hit	*render_cylinder(const t_obj *obj,
 	t_quadratic_equation	abc;
 	t_obj_ray_hit			*obj_hit;
 	double					t_param[2];
+	double					t_caps[2];
 	double					dis;
 
 	abc = cylinder_get_quad_abc(obj, ray_origin, v_ray);
@@ -90,12 +116,15 @@ t_obj_ray_hit	*render_cylinder(const t_obj *obj,
 			(-abc.b + sqrt(dis)) / (2 * abc.a));
 	t_param[1] = cylinder_limit_height(ray_origin, obj, v_ray,
 			(-abc.b - sqrt(dis)) / (2 * abc.a));
-	if (dis < 0 || !cylinder_calc_t_param_hit(t_param))
+	cylinder_caps(t_caps, obj, ray_origin, v_ray);
+	t_param[0] = t_param[cylinder_calc_t_param_hit(t_param)];
+	t_param[1] = t_caps[cylinder_calc_t_param_hit(t_caps)];
+	if (t_param[cylinder_calc_t_param_hit(t_param)] < 0)
 		return (0);
 	obj_hit = malloc(sizeof(t_obj_ray_hit));
 	if (!obj_hit)
 		return (0);
-	obj_hit->t = t_param[cylinder_calc_t_param_hit(t_param) - 1];
+	obj_hit->t = t_param[cylinder_calc_t_param_hit(t_param)];
 	obj_hit->obj = obj;
 	return (obj_hit);
 }
