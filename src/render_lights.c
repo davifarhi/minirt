389ed0811616fdbb64fefed9961ad1a41a58f447
Michele-Reymond <hello@michelereymond.ch>
2022-10-20 21:35:01 +0200

    Multilights en cours

--- src/render_lights.c
+++ src/render_lights.c
@@ -6,7 +6,7 @@
 /*   By: mreymond <mreymond@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 13:52:30 by davifah           #+#    #+#             */
-/*   Updated: 2022/10/20 18:45:04 by mreymond         ###   ########.fr       */
+/*   Updated: 2022/10/20 21:27:14 by mreymond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,22 @@ static float	spot_light(t_parse *data,
 	return (light_value);
 }
 
+float	spot_light_test(t_coord l_coord, double l_brightness,
+	t_coord point, t_vector normal, t_vector light)
+{
+	double		scalaire;
+	float		light_value;
+
+	light_value = 0.0;
+	scalaire = dot_product(normal, light);
+	if (scalaire > 0)
+	{
+		light_value = (l_brightness * scalaire)
+			/ distance(&point, &l_coord);
+	}
+	return (light_value);
+}
+
 int	render_light(t_parse *data, t_obj_ray_hit *obj_hit, t_vector v_ray)
 {
 	int			light_value;
@@ -78,9 +94,77 @@ int	render_light(t_parse *data, t_obj_ray_hit *obj_hit, t_vector v_ray)
 	light_value = multiply_light(obj_hit->obj->color,
 			light_indice, data->light_color);
 	light_indice = specular_light(data, obj_hit, v_ray, light);
-	light_value = additive_light(light_value, light_indice, data->light_color)
-		* is_in_shadow(light, point, data);
+	if (is_in_shadow(light, point, data) != 0)
+		light_value = additive_light(light_value, light_indice, data->light_color);
+	else	
+		light_value = 0;
+
+	t_coord			l_coord;
+	double			l_brightness;
+	int				l_color;
+	int			old_value;
+	old_value = light_value;
+	l_coord.x = 25;
+	l_coord.y = 75;
+	l_coord.z = -95;
+	l_brightness = 1;
+	l_color = create_trgb(1, 0, 0, 255);
+	light = v_sub(*(t_vector *)&l_coord, *(t_vector *)&point);
+	light_indice = spot_light_test(l_coord, l_brightness, point, normal, light);
+	light_value = additive_light(light_value, light_indice, l_color);
+	light_indice = specular_light(data, obj_hit, v_ray, light);
+	if (is_in_shadow(light, point, data) != 0)
+		light_value = additive_light(light_value, light_indice, l_color);
+	else	
+		light_value = old_value;
+
+
+
+	old_value = light_value;
+	l_coord.x = 0;
+	l_coord.y = 75;
+	l_coord.z = -25;
+	l_brightness = 1;
+	l_color = create_trgb(1, 0, 255, 0);
+	light = v_sub(*(t_vector *)&l_coord, *(t_vector *)&point);
+	light_indice = spot_light_test(l_coord, l_brightness, point, normal, light);
+	light_value = additive_light(light_value, light_indice, l_color);
+	light_indice = specular_light(data, obj_hit, v_ray, light);
+	if (is_in_shadow(light, point, data) != 0)
+		light_value = additive_light(light_value, light_indice, l_color);
+	else	
+		light_value = old_value;
+
+
+			
+	
 	light_value = additive_light(light_value,
 			data->ambient_intensity, data->ambient_color);
 	return (light_value);
 }
+
+// int	render_light(t_parse *data, t_obj_ray_hit *obj_hit, t_vector v_ray)
+// {
+// 	int			light_value;
+// 	float		light_indice;
+// 	t_coord		point;
+// 	t_vector	normal;
+// 	t_vector	light;
+
+// 	point = hit_point(data->cam_coord, obj_hit, v_ray);
+// 	light = v_sub(*(t_vector *)&data->light_coord, *(t_vector *)&point);
+// 	normal = find_normal_vector(point, obj_hit);
+// 	v_normalize(&normal);
+// 	point.x = point.x - v_ray.x * LEN;
+// 	point.y = point.y - v_ray.y * LEN;
+// 	point.z = point.z - v_ray.z * LEN;
+// 	light_indice = spot_light(data, point, normal, light);
+// 	light_value = multiply_light(obj_hit->obj->color,
+// 			light_indice, data->light_color);
+// 	light_indice = specular_light(data, obj_hit, v_ray, light);
+// 	light_value = additive_light(light_value, light_indice, data->light_color)
+// 		* is_in_shadow(light, point, data);
+// 	light_value = additive_light(light_value,
+// 			data->ambient_intensity, data->ambient_color);
+// 	return (light_value);
+// }
